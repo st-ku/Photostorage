7136322a57a82b933628916075de532300822a53
st-ku <lecompt.by@gmail.com>
2020-12-29 19:16:14 +0300

    init commit

---
+++ src/main/java/freeblr/photostorage/PhotostorageApplication.java
@@ -0,0 +1,11 @@
+package freeblr.photostorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotostorageApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PhotostorageApplication.class, args);
+    }
+}
---
+++ src/test/java/freeit/photostorage/PhotostorageTest.java
@@ -0,0 +1,13 @@
+package freeit.photostorage;
+
+import freeblr.photostorage.PhotostorageApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = PhotostorageApplication.class)
+public class PhotostorageTest {
+    @Test
+    void contextLoads() {
+    }
+
+}
