f04758720f683a6f33ddd03d85f69d4a2272bd48
st-ku <lecompt.by@gmail.com>
2020-12-30 23:14:14 +0300

    fixes

--- src/main/java/freeblr/photostorage/controller/PhotoController.java
+++ src/main/java/freeblr/photostorage/controller/PhotoController.java
@@ -2,10 +2,7 @@ package freeblr.photostorage.controller;
 
 import freeblr.photostorage.model.Picture;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.List;
@@ -38,26 +35,12 @@ public class PhotoController {
 
     @GetMapping("/{id}")
     public ResponseEntity<byte[]> getPicture(@PathVariable Long id) throws IOException {
-
-        //some path = path + picture.getName(), for example
-
-        /*File imgPath = new File("some path");
-        byte[] image = Files.readAllBytes(imgPath.toPath());
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.IMAGE_JPEG);
-        headers.setContentLength(image.length);
-        return new ResponseEntity<>(image, headers, HttpStatus.OK);*/
-
         return null;
     }
 
-    /*
-    Need to add for correct work (in future) - if we will return files instead of links
-    @Override
-    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-    ByteArrayHttpMessageConverter byteConverter = new ByteArrayHttpMessageConverter();
-    converters.add(byteConverter);
-    super.configureMessageConverters(converters);
-}
-     */
+    @PostMapping("/photos")
+    public void savePicture() throws IOException {
+
+    }
+
 }
--- src/main/java/freeblr/photostorage/model/Picture.java
+++ src/main/java/freeblr/photostorage/model/Picture.java
@@ -1,5 +1,10 @@
 package freeblr.photostorage.model;
 
+import jdk.jfr.DataAmount;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 import java.util.Objects;
 
@@ -20,6 +25,9 @@ import java.util.Objects;
  * </code></p>
  * After add lombok it will stay more compact, and Builder will be deleted after adding repo
  */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Picture {
 
     private Long id;
@@ -30,132 +38,5 @@ public class Picture {
     private String description;
     private LocalDateTime created;
     private LocalDateTime updated;
-    //CloudinaryObj temporarily absent
-
-    public static class Builder {
-
-        private final Picture picture = new Picture();
-
-        public Builder withId(Long id) {
-            this.picture.id = id;
-            return this;
-        }
-
-        public Builder withWidth(int width) {
-            this.picture.width = width;
-            return this;
-        }
-
-        public Builder withHeight(int height) {
-            this.picture.height = height;
-            return this;
-        }
-
-        public Builder withSize(long size) {
-            this.picture.size = size;
-            return this;
-        }
-
-        public Builder withType(String type) {
-            this.picture.type = type;
-            return this;
-        }
-
-        public Builder withDescription(String description) {
-            this.picture.description = description;
-            return this;
-        }
-
-        public Builder createdAt(LocalDateTime created) {
-            this.picture.created = created;
-            return this;
-        }
-
-        public Builder updatedAt(LocalDateTime updated) {
-            this.picture.updated = updated;
-            return this;
-        }
-
-        public Picture build() {
-            return picture;
-        }
-    }
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public int getWidth() {
-        return width;
-    }
-
-    public void setWidth(int width) {
-        this.width = width;
-    }
-
-    public int getHeight() {
-        return height;
-    }
-
-    public void setHeight(int height) {
-        this.height = height;
-    }
-
-    public long getSize() {
-        return size;
-    }
-
-    public void setSize(long size) {
-        this.size = size;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public LocalDateTime getCreated() {
-        return created;
-    }
-
-    public void setCreated(LocalDateTime created) {
-        this.created = created;
-    }
-
-    public LocalDateTime getUpdated() {
-        return updated;
-    }
-
-    public void setUpdated(LocalDateTime updated) {
-        this.updated = updated;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Picture picture = (Picture) o;
-        return id.equals(picture.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
+    private String url;
 }
