a519bf2856b6b141e8f20d216ace06a51ce60583
Mikhail Kotor <milan3020@gmail.com>
2021-01-04 15:27:01 +0000

    PHOTOSTORAGE-T-14
    Co-authored-by: Stanislau Kurstak <lecompt.by@gmail.com>

--- src/main/java/info/freeit/photostorage/PhotoStorageApplication.java
+++ src/main/java/info/freeit/photostorage/PhotoStorageApplication.java
@@ -2,8 +2,10 @@ package info.freeit.photostorage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class PhotoStorageApplication {
     public static void main(String[] args) {
         SpringApplication.run(PhotoStorageApplication.class, args);
--- src/main/java/info/freeit/photostorage/model/Picture.java
+++ src/main/java/info/freeit/photostorage/model/Picture.java
@@ -3,18 +3,29 @@ package info.freeit.photostorage.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Picture {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private int width;
     private int height;
     private String type;
+    @CreationTimestamp
     private LocalDateTime created;
+    @UpdateTimestamp
     private LocalDateTime updated;
     private String url;
     private String metaData;
