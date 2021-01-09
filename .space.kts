0a42272bb57edc239d59b74ba3b2c386f11d55da
Mikhail Kotor <milan3020@gmail.com>
2021-01-09 23:40:16 +0000

    ci added
    Co-authored-by: a.shulga <a.shulga@complitech.org>

--- .space.kts
+++ .space.kts
@@ -4,6 +4,25 @@
 * For more info, see https://www.jetbrains.com/help/space/automation.html
 */
 
-job("Hello World!") {
-    container("hello-world")
+job("build") {
+    container("openjdk:11") {
+        resources {
+            // 500.mcpu = 0.5.cpu
+            cpu = 0.4.cpu
+            // 1500 MB
+            memory = 1500.mb
+        }
+        shellScript {
+            content = """
+                    apt-get update
+                    apt-get -y install maven
+                    mvn --version
+                    mvn package
+                    cp target $mountDir/share
+                    ls -la $mountDir/share
+                    mvn clean package
+                """
+        }
+    }
+
 }
